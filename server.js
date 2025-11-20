e0a98806b2c6dc25fb04536fc8ae2f355295ee95
Mare <odenosemare.okoigun@ontariotechu.net>
2025-11-19 23:13:55 -0500

    fourth commit

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-let http = require('http');
-let app = require('./app');
+const app = require('./app');
 
-let port = process.env.PORT || 3000;
-app.set('port', port);
+// Render provides a PORT in process.env.PORT
+const PORT = process.env.PORT || 3000;
 
-let server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
